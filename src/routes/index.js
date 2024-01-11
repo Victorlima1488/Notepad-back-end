e7b4f051e8e35ed4af4b0f0b21f7acef4e8b500a
victorlima1488 <victorlima.softwaredeveloper@gmail.com>
2024-01-10 22:34:32 -0300

    feat: new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const userRoutes = require("./users.routes")
 const noteRoutes = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 
 routes.use("/users", userRoutes)
 routes.use("/notes", noteRoutes)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
